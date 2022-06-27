0d35d5dcd68a91b482b253f96b53f5a1965df7db
liakhim <mmingareev@bk.ru>
2022-06-27 23:50:37 +0300

    qweqwe

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'aim' );
+define( 'DB_NAME', 'ingustos_bd' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
