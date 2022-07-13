d6286d0873930a14643c6b5c4bb3c1d2f71afbcb
liakhim <mmingareev@bk.ru>
2022-07-14 02:01:53 +0300

    hg

--- layout/router/products.php
+++ layout/router/products.php
@@ -12,7 +12,18 @@ $woocommerce = new Client(
         'version' => 'wc/v3'
     ]
 );
+$products = $woocommerce->get('products');
+$finalProducts = [];
+foreach ($products as $product) {
+    foreach ($product->variations as $variation_id) {
+        $variation = $woocommerce->get('products/30/variations/'.$variation_id);
+        if (count($variation->attributes)) {
+            $product->vars[] = $variation;
+        }
+    }
+    $finalProducts[] = $product;
+}
 header("Content-Type: application/json");
-echo json_encode($woocommerce->get('products'));
+echo json_encode($finalProducts);
 // exit();
 ?>
